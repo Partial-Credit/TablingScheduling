c26ce5cc7ed030a9d50ef2f29d809183178da142
KristoferKwan <35852328+KristoferKwan@users.noreply.github.com>
2019-01-11 10:48:46 -0500

    ADD: web scraping capabilities to program script

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # TablingScheduling
 A simple tabling scheduling program that takes multiple people's available
-times, and outputs a fair schedule of members at the table, within the
+times through scraping WhenIsGood.net, and outputs a fair schedule of members at the table, within the
 constraints given by each member's available times.
 
 Makes use of dynamic programming to get as close to the optimal fairness
@@ -8,7 +8,4 @@ solution as possible.
 
 Partial Credit is the best!
 # Usage
-``` python main.py <available_times.csv> <# of people at table> <length of shift in half hours>```
-
-Future developments will crawl scheduling websites
-like whenisgiood.net to get the data automatically from the website.
+``` python main.py <WhenIsGood URL> <# of people at table> <length of shift in half hours>```
--- main.py
+++ main.py
@@ -4,6 +4,84 @@ import datetime
 import copy
 import random
 import numpy as np
+from bs4 import BeautifulSoup 
+import urllib.request
+import time as time_funct
+
+
+def delete_escape_chars(string):
+    string= string.decode("utf-8")
+    string = string.replace("\r", "").replace("\n", "").replace("\t", "")
+    string_list = string.split(",")
+    return string_list
+
+def whenIsGoodScrapper(whenIsGoodUrl):
+    try:
+        req = urllib.request.Request(whenIsGoodUrl)
+        html  =  urllib.request.urlopen(req).read()
+        print("Parsing through WhenIsGood..")
+    except Exception as ex:
+        print("could not open html file! Not a valid site!")
+
+    try:
+        soup = BeautifulSoup(html, 'html.parser')
+        data = html.decode("utf-8", errors='replace')
+    except Exception as ex:
+        print("cant decode char")
+    time_slots = soup.find_all('td', ["proposed"])
+    people = dict()
+    dates = set()
+    count = 0
+    for time in time_slots:
+        slot = dict()
+        url = "http://whenisgood.net" + time['onclick'].replace("pop(", "").replace("'", "").replace(",resultspopup);", "")
+        req_slot = urllib.request.Request(url)
+        html_slot  =  urllib.request.urlopen(req_slot).read()
+        soup_slot = BeautifulSoup(html_slot, 'html.parser')
+        labels = soup_slot.find_all('tr')
+        
+        date = (labels[1].find('td', {"class": "field"}).renderContents()).decode('utf-8')
+        time = (labels[2].find('td', {"class": "field"}).renderContents()).decode('utf-8')
+        can_make_it = delete_escape_chars(labels[3].find('td', {"class": "field"}).renderContents())
+        cant_make_it = delete_escape_chars(labels[4].find('td', {"class": "field"}).renderContents())
+        
+        dates.add(date)
+        for person in can_make_it:
+            try:
+                people[person]
+            except:
+                people[person] = dict()
+            try:
+                people[person][time]
+            except:
+                people[person][time] = dict()  
+            people[person][time][date] = "TRUE"
+        for person in cant_make_it:
+            try:
+                people[person]
+            except:
+                people[person] = dict()
+            try:
+                people[person][time]
+            except:
+                people[person][time] = dict()      
+            people[person][time][date] = "FALSE"
+    
+    dates_object = [datetime.datetime.strptime(ts, "%B %d, %Y") for ts in dates]
+    dates_object.sort()
+    sorteddates = [datetime.datetime.strftime(ts, "%B %d %Y") for ts in dates_object]
+    
+    with open('input_file.csv', mode = 'w') as table_file:
+        table_writer = csv.writer(table_file, delimiter=',', quotechar='"',lineterminator='\n',quoting=csv.QUOTE_MINIMAL)
+        for person in people:
+            table_writer.writerow([person] + sorteddates)
+            for time in people[person]:
+                temp = []
+                for date in people[person][time]:
+                    temp.append(people[person][time][date])
+                table_writer.writerow([time] + temp)
+    return
+
 
 def make_dict(schedule, members, predict = None):
     if not predict is None:
@@ -36,9 +114,9 @@ def equality(schedule, members, predict = None, itr = 1):
 def schedule(days, slots_per_day, members, member_availability, predict = None, itr = 1):
     N = 300
     
-    possible_solutions = [[False] * days * slots_per_day]
+    possible_solutions = [[False] * round(days * slots_per_day)]
 
-    for t in range(slots_per_day * days):
+    for t in range(round(slots_per_day * days)):
         eq = equality(possible_solutions[0], members, predict, itr)
         new_solutions = []
         choose = list(range(len(members)))
@@ -46,7 +124,6 @@ def schedule(days, slots_per_day, members, member_availability, predict = None, 
             R = random.randint(0, len(choose) - 1)
             m = choose[R]
             del choose[R]
-            
             if member_availability[m][t]:
                 for S in possible_solutions:
                     s = copy.deepcopy(S)
@@ -71,7 +148,8 @@ def schedule(days, slots_per_day, members, member_availability, predict = None, 
     return possible_solutions
 
 if __name__ == "__main__":
-    filename = sys.argv[1]
+    filename = "input_file.csv"
+    url = sys.argv[1]
     at_table = int(sys.argv[2])
     times = int(sys.argv[3])
     members = []
@@ -79,13 +157,15 @@ if __name__ == "__main__":
     day_values = []
     start = None
     
+    whenIsGoodScrapper(url)
+
     with open(filename) as f:
-        reader = csv.reader(f, delimiter=',', quotechar='|')
+        reader = csv.reader(f, delimiter=',', quotechar='|', lineterminator='\n', quoting=csv.QUOTE_MINIMAL)
         days = 0
         slots_per_day = 0
         for row in reader:
             try:
-                T = datetime.datetime.strptime(row[0], "%H:%M")
+                T = datetime.datetime.strptime(row[0], "%I:%M:%S %p")
                 if start is None:
                     start = T
                     
@@ -103,7 +183,6 @@ if __name__ == "__main__":
                     days = len(row) - 1
                     day_values = row[1:]
                     slots_per_day = 0
-                    
                 members.append(row[0])
                 new_times = []
                 for i in range(days):
@@ -129,9 +208,9 @@ if __name__ == "__main__":
             
             t += times
             
-    print "Making tabling schedule for the following members:"
-    print members
-    print ""
+    print("Making tabling schedule for the following members:")
+    print(members)
+    print("")
     S = []
     D = None
     for i in range(at_table):
@@ -147,16 +226,16 @@ if __name__ == "__main__":
         C = csv.writer(f)
         full_schedule = np.column_stack(S)
         C.writerow([''] + day_values)
-        for t in range(slots_per_day):
+        for t in range(int(slots_per_day)):
             row = []
             curr_time = start + datetime.timedelta(minutes=30) * times * (t % slots_per_day)
             row.append(curr_time.strftime("%H:%M"))
             for d in range(days):
                 string = ""
-                for s in full_schedule[d * slots_per_day + t]:
+                for s in full_schedule[d * int(slots_per_day) + t]:
                     string += s + " "
                 row.append(string)
-            print row
+            print(row)
             C.writerow(row)
         
-    print D
+    print(D)
